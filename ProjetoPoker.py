0b8230bdd3fd520d1921a8ca0d506637d295c3eb
Arthur Alvarenga Malospirito <arthur.escola.200608@gmail.com>
2025-04-06 16:08:55 -0400

    Update ProjetoPoker.py

--- ProjetoPoker.py
+++ ProjetoPoker.py
@@ -14,7 +14,7 @@ listaStraight=[["A","2","3","4","5"],["6","2","3","4","5"],["6","7","3","4","5"]
 
 class players:
     
-    def __init__(self,cardsReal,cards,iOuro,iEspada,iCopas,iPaus,mao,iMaiorQtd,nome,stack,funcao,aposta):
+    def __init__(self,cardsReal,cards,iOuro,iEspada,iCopas,iPaus,mao,iMaiorQtd,nome,stack,funcao,aposta,check,fold):
         self.cardsReal: list = cardsReal #Cartas de forma bonitinha:  2♦, 9♠...
         self.cards: list = cards #Valor númerico das cartas: A,2,3,4...Q,K
         self.iOuro: int = iOuro #contador do naipe
@@ -27,12 +27,14 @@ class players:
         self.stack: int =stack
         self.funcao: str = funcao
         self.aposta: int = aposta
+        self.check: bool = check
+        self.fold: bool = fold
 
 
 def novaCarta(player,carta): #Adiciona nova carta ao player, coloca o valor na lista de Card e o naipe no seu respectivo contador, e as cartas reais na lista visual
     valorCartaAdd =""
     cartaNova = carta
-    if len(player.cardsReal)<=2:
+    if len(player.cardsReal)<2:
         player.cardsReal.append(cartaNova)
     for caractere in cartaNova:
         if caractere in algarismos:
@@ -62,6 +64,7 @@ def verifyMao(player):
         
         if all(elem in player.cards for elem in listaRTF):
             player.mao.insert(0,"RSF")
+            player.mao.insert(3,10)
             #return print("Royal Straight Flush")
             RSF=True
 
@@ -70,6 +73,7 @@ def verifyMao(player):
                 if all(elem in player.cards for elem in listaStraight[i]):
                     player.mao.insert(0,"SF")
                     player.mao.insert(1,i)
+                    player.mao.insert(3,9)
                     #return print("Straight Flush")
                     SF=True
                 
@@ -89,6 +93,7 @@ def verifyMao(player):
                     if letra==naipeTemp:
                         cartasDoFlush.append(carta)
             player.mao.insert(1,cartasDoFlush)
+            player.mao.insert(3,6)
             #return print("Flush")
     
     else: 
@@ -99,6 +104,7 @@ def verifyMao(player):
             if all(elem in player.cards for elem in listaStraight[i]):
                 player.mao.insert(0,"straight")
                 player.mao.insert(1,i)
+                player.mao.insert(3,5)
                 #return print("Straight")
                 straight=True
             
@@ -106,28 +112,33 @@ def verifyMao(player):
             if player.iMaiorQtd[0][1]>=4:
                 player.mao.insert(0,"poker")
                 player.mao.insert(1,player.iMaiorQtd[0][0])
+                player.mao.insert(3,8)
                 #return print("Poker")
             
             elif player.iMaiorQtd[0][1]>=3 and player.iMaiorQtd[1][1]>=2:
                 player.mao.insert(0,"FH")
                 player.mao.insert(1,player.iMaiorQtd[0][0])
                 player.mao.insert(2,player.iMaiorQtd[1][0])
+                player.mao.insert(3,7)
                 #return print("Full House")
             
             elif player.iMaiorQtd[0][1]>=3:
                 player.mao.insert(0,"trio")
                 player.mao.insert(1,player.iMaiorQtd[0][0])
+                player.mao.insert(3,4)
                 #return print("Trio")
             
             elif player.iMaiorQtd[0][1]>=2 and player.iMaiorQtd[1][1]>=2:
                 player.mao.insert(0,"2par")
                 player.mao.insert(1,player.iMaiorQtd[0][0])
                 player.mao.insert(2,player.iMaiorQtd[1][0])
+                player.mao.insert(3,3)
                 #return print("Dois Pares")
             
             elif player.iMaiorQtd[0][1]>=2:
                 player.mao.insert(0,"par")
                 player.mao.insert(1,player.iMaiorQtd[0][0])
+                player.mao.insert(3,2)
                 #return print("Par")
             
             else:
@@ -137,11 +148,29 @@ def verifyMao(player):
                     if ordemCartas.index(carta)>ordemCartas.index(HC):
                         HC = carta
                 player.mao.insert(1,HC)
+                player.mao.insert(3,1)
                 #return print("High Card")
     
+def decidirGanhador(listaDeJogadores:list):
+    ganhador=listaDeJogadores[0].mao[3]
+    empate=[]
+    for player in listaDeJogadores:
+        if player.mao[3]<ganhador:
+            pass
+        elif player.mao[3]==ganhador:
+            empate.append(player)
+        else:
+            ganhador = player.mao[3]
+    
+    if len(empate)>=2:
+        print("Desempate")
+    else:
+        return ganhador
     
 
-player1 = players([],[],0,0,0,0,[],[],"",1000,"",0)
+    
+
+player1 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Teste rápido para verificar mãos ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -197,13 +226,13 @@ player1 = players([],[],0,0,0,0,[],[],"",1000,"",0)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ JOGO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #player1 = players([],[],0,0,0,0,[],[])
-player2 = players([],[],0,0,0,0,[],[],"",1000,"",0)
-player3 = players([],[],0,0,0,0,[],[],"",1000,"",0)
-player4 = players([],[],0,0,0,0,[],[],"",1000,"",0)
-player5 = players([],[],0,0,0,0,[],[],"",1000,"",0)
-player6 = players([],[],0,0,0,0,[],[],"",1000,"",0)
-player7 = players([],[],0,0,0,0,[],[],"",1000,"",0)
-player8 = players([],[],0,0,0,0,[],[],"",1000,"",0)
+player2 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
+player3 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
+player4 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
+player5 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
+player6 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
+player7 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
+player8 = players([],[],0,0,0,0,[],[],"",1000,"",0,False,False)
 
 listPlayersOriginal = [player1,player2,player3,player4,player5,player6,player7,player8]
 ofcOrdemPlayers = [0,1,2,3,4,5,6,7]
@@ -252,63 +281,184 @@ input("Começar Rodada?\n")
 listPlayers[ordemPlayers[0]].funcao = "SB"
 listPlayers[ordemPlayers[1]].funcao = "BB" 
 input(f"Mostrar {listPlayers[ordemPlayers[0]].nome}?\n")
-maiorAposta = 0
+qtdJogador = len(listPlayers)
+ganhouPorFold=False
+contadorJogador = 0
+primeiroTruno =True
+ultimaRodada = False
+rodada = 1
+contadorFold=0
+maiorAposta = 5
 pot = 0
-for i in ordemPlayers:
-    if i==0:
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        print(f"{listPlayers[ordemPlayers[i]].nome}, você é o Small Blind\n")
-        print(f"{listPlayers[ordemPlayers[i]].cardsReal}\n")
-        print("A aposta mínima da mesa inicial é 5\n")
-        print(f"O pote é: {pot}\n")
-        #listPlayers[ordemPlayers[i]].aposta
-        apostaTemp = int(input("Digite sua aposta:\n"))
-        apostaCerta=False
-        while apostaCerta==False:
-            if apostaTemp<5:
-                print("A aposta deve ser maior ou igual a 5")
+while rodada<=3:
+    for i in ordemPlayers:
+        if primeiroTruno==True:
+            if i==0:
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                print(f"{listPlayers[ordemPlayers[i]].nome}, você é o Small Blind\n")
+                print(f"{listPlayers[ordemPlayers[i]].cardsReal}\n")
+                print(f"A aposta mínima da mesa inicial é {maiorAposta}\n")
+                print(f"O pote é: {pot}\n")
+                #listPlayers[ordemPlayers[i]].aposta
                 apostaTemp = int(input("Digite sua aposta:\n"))
-            else:
-                apostaCerta=True
-        listPlayers[ordemPlayers[i]].aposta = apostaTemp
-        maiorAposta = apostaTemp
-        pot+=apostaTemp
-    elif i==1:
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        input(f"Mostrar {listPlayers[ordemPlayers[i]].nome}?\n")
-        print(f"{listPlayers[ordemPlayers[i]].nome}, você é o Big Blind\n")
-        print(f"{listPlayers[ordemPlayers[i]].cardsReal}\n")
-        print(f"A aposta mínima da mesa é {maiorAposta*2}\n")
-        print(f"O pote é: {pot}\n")
-        apostaTemp = int(input("Digite sua aposta:\n"))
-        apostaCerta=False
-        while apostaCerta==False:
-            if apostaTemp<maiorAposta*2:
-                print(f"A aposta deve ser maior ou igual a {maiorAposta*2}")
+                apostaCerta=False
+                while apostaCerta==False:
+                    if apostaTemp<maiorAposta:
+                        print("A aposta deve ser maior ou igual a 5")
+                        apostaTemp = int(input("Digite sua aposta:\n"))
+                    else:
+                        apostaCerta=True
+                listPlayers[ordemPlayers[i]].aposta = apostaTemp
+                maiorAposta = apostaTemp
+                pot+=apostaTemp
+                contadorJogador+=1
+            elif i==1:
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                input(f"Mostrar {listPlayers[ordemPlayers[i]].nome}?\n")
+                print(f"{listPlayers[ordemPlayers[i]].nome}, você é o Big Blind\n")
+                print(f"{listPlayers[ordemPlayers[i]].cardsReal}\n")
+                print(f"A aposta mínima da mesa é {maiorAposta*2}\n")
+                print(f"O pote é: {pot}\n")
                 apostaTemp = int(input("Digite sua aposta:\n"))
+                apostaCerta=False
+                while apostaCerta==False:
+                    if apostaTemp<maiorAposta*2:
+                        print(f"A aposta deve ser maior ou igual a {maiorAposta*2}")
+                        apostaTemp = int(input("Digite sua aposta:\n"))
+                    else:
+                        apostaCerta=True
+                listPlayers[ordemPlayers[i]].aposta = apostaTemp
+                maiorAposta = apostaTemp
+                pot+=apostaTemp
+                contadorJogador+=1
             else:
-                apostaCerta=True
-        listPlayers[ordemPlayers[i]].aposta = apostaTemp
-        maiorAposta = apostaTemp
-        pot+=apostaTemp
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                input(f"Mostrar {listPlayers[ordemPlayers[i]].nome}?\n")
+                print(f"{listPlayers[ordemPlayers[i]].nome}, É a sua Vez\n")
+                print(f"{listPlayers[ordemPlayers[i]].cardsReal}\n")
+                print(f"A aposta mínima da mesa é {maiorAposta}\n")
+                print(f"O pote é: {pot}\n")
+                escolha = input("Qual sua escolha?\n'Check', 'Fold' ou 'Raise'\n").lower()
+                escolhaCerta =False
+                while escolhaCerta==False:
+                    if escolha=="check" or escolha=="fold" or escolha=="raise":
+                        escolhaCerta=True
+                    else:
+                        print("\nEscolha indisponível!\n")
+                        escolha = input("Qual sua escolha?\nCheck, Fold, Raise?\n").lower()
+                        
+                if escolha=="raise":
+                    apostaTemp = int(input("Digite sua aposta:\n"))
+                    apostaCerta=False
+                    while apostaCerta==False:
+                        if apostaTemp<maiorAposta:
+                            print(f"A aposta deve ser maior ou igual a {maiorAposta}")
+                            apostaTemp = int(input("Digite sua aposta:\n"))
+                        else:
+                            apostaCerta=True
+                
+                    listPlayers[ordemPlayers[i]].aposta = apostaTemp
+                    maiorAposta = apostaTemp
+                    pot+=apostaTemp
+                elif escolha=="check":
+                    if listPlayers[ordemPlayers[i]].aposta<maiorAposta:
+                        pot+=maiorAposta-listPlayers[ordemPlayers[i]].aposta
+                        listPlayers[ordemPlayers[i]].aposta = maiorAposta
+
+                        listPlayers[ordemPlayers[i]].check = True
+                else:
+                    listPlayers[ordemPlayers[i]].fold = True
+                    contadorFold+=1
+                contadorJogador+=1
+            if contadorJogador==qtdJogador:
+                primeiroTruno=False
+        else:
+            if listPlayers[ordemPlayers[i]].fold==False:
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                input(f"Mostrar {listPlayers[ordemPlayers[i]].nome}?\n")
+                print(f"{listPlayers[ordemPlayers[i]].nome}, É a sua Vez\n")
+                print(f"Suas Cartas são:\n{listPlayers[ordemPlayers[i]].cardsReal}\n")
+                print(f"A mesa é:\n {mesa}\n")
+                print(f"A aposta mínima da mesa é {maiorAposta}\n")
+                print(f"O pote é: {pot}\n")
+                escolha = input("Qual sua escolha?\n'Check', 'Fold' ou 'Raise'\n").lower()
+                escolhaCerta =False
+                while escolhaCerta==False:
+                    if escolha=="check" or escolha=="fold" or escolha=="raise":
+                        escolhaCerta=True
+                    else:
+                        print("\nEscolha indisponível!\n")
+                        escolha = input("Qual sua escolha?\nCheck, Fold, Raise?\n").lower()
+                        
+                if escolha=="raise":
+                    apostaTemp = int(input("Digite sua aposta:\n"))
+                    apostaCerta=False
+                    while apostaCerta==False:
+                        if apostaTemp<maiorAposta:
+                            print(f"A aposta deve ser maior ou igual a {maiorAposta}")
+                            apostaTemp = int(input("Digite sua aposta:\n"))
+                        else:
+                            apostaCerta=True
+                
+                    listPlayers[ordemPlayers[i]].aposta = apostaTemp
+                    maiorAposta = apostaTemp
+                    pot+=apostaTemp
+                elif escolha=="check":
+                    if listPlayers[ordemPlayers[i]].aposta<maiorAposta:
+                        pot+=maiorAposta-listPlayers[ordemPlayers[i]].aposta
+                        listPlayers[ordemPlayers[i]].aposta = maiorAposta
+
+                        listPlayers[ordemPlayers[i]].check = True
+                else:
+                    listPlayers[ordemPlayers[i]].fold = True
+                    contadorFold+=1
+    if rodada==1:
+        for j in range(0,3):
+                cartaNovaMesa = random.choice(cartasBaralhoRest)
+                mesa.append(cartaNovaMesa)
+                cartasBaralhoRest.remove(cartaNovaMesa)
+        for player in listPlayers:
+            for carta in mesa:
+                novaCarta(player,carta)
+        rodada+=1
+    elif rodada==2:
+        cartaNovaMesa = random.choice(cartasBaralhoRest)
+        mesa.append(cartaNovaMesa)
+        cartasBaralhoRest.remove(cartaNovaMesa)
+        for player in listPlayers:
+            novaCarta(player,cartaNovaMesa)
+        rodada+=1
     else:
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        input(f"Mostrar {listPlayers[ordemPlayers[i]].nome}?\n")
-        print(f"{listPlayers[ordemPlayers[i]].nome}, É a sua Vez\n")
-        print(f"{listPlayers[ordemPlayers[i]].cardsReal}\n")
-        print(f"A aposta mínima da mesa é {maiorAposta}\n")
-        print(f"O pote é: {pot}\n")
-        apostaTemp = int(input("Digite sua aposta:\n"))
-        apostaCerta=False
-        while apostaCerta==False:
-            if apostaTemp<maiorAposta:
-                print(f"A aposta deve ser maior ou igual a {maiorAposta}")
-                apostaTemp = int(input("Digite sua aposta:\n"))
-            else:
-                apostaCerta=True
-        listPlayers[ordemPlayers[i]].aposta = apostaTemp
-        maiorAposta = apostaTemp
-        pot+=apostaTemp
+        cartaNovaMesa = random.choice(cartasBaralhoRest)
+        mesa.append(cartaNovaMesa)
+        cartasBaralhoRest.remove(cartaNovaMesa)
+        for player in listPlayers:
+            novaCarta(player,cartaNovaMesa)
+        rodada+=1
     
+    if len(listPlayers)-contadorFold<2:
+        ganhouPorFold=True
+        break
+
+ListaDeVencedores =[]
+
+print("Acabou!")
+if ganhouPorFold==True:
+    for player in listPlayers:
+        if player.fold==False:
+            ganhador = player
+else:
+    for player in listPlayers:
+        if player.fold==False:
+            verifyMao(player)
+            ListaDeVencedores.append(player)
+    ganhador = decidirGanhador(ListaDeVencedores)
+
+    
+
+print(f"O ganhador foi {ganhador.nome}")
+
+
+
 
 #1. Função que verifica desempate, mesma coisa da mão normal, mas ignora a mão inicial e vai na segunda.
